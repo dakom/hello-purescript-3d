18a3799ceaf423279db8b9dd36f98b1c112aaee7
David Komer <david.komer@gmail.com>
2018-07-27 00:24:52 +0300

    working!

--- src/app/IO.ts
+++ src/app/IO.ts
@@ -1,16 +1,13 @@
 import MyWorker = require('worker-loader!./Worker');
-import {gltf_load, GltfBridge, GltfScene, LightKind, createVec3, createQuat, GltfLightNode, NodeKind, getMatrixFromTrs} from "pure3d";
+import {gltf_load, GltfBridge, GltfScene} from "pure3d";
 import { getCompileFlags, loadWorker} from "utils/Utils";
 import {WorkerCommand, MESSAGE} from "types/Worker-Types";
 import {createRenderer} from "./io/renderer/Renderer";
-import {createCamera} from "./io/camera/Camera";
+import {prepScene} from "./io/scene/Scene-Prep";
 import {parallel} from "fluture";
+
 const {buildMode, buildVersion, isProduction} = getCompileFlags();
 console.log(`%c Purescript Hello World ${buildVersion} (productionMode: ${isProduction})`, 'color: #4286f4; font-size: large; font-family: "Comic Sans MS", cursive, sans-serif');
-
-/*
- * Everything here is generic boilerplate... the details are in the imports
- */
 
 const renderer = createRenderer();
 
@@ -18,8 +15,7 @@ parallel (Infinity) ([
     loadWorker(new (MyWorker as any)()),
     gltf_load({
         renderer, 
-        path: "static/ball/scene.gltf"
-//        path: "static/cube/Cube.gltf"
+        path: "static/hello-world/HelloWorld.glb"
     })
 ]).fork(
         console.error,
@@ -35,36 +31,10 @@ parallel (Infinity) ([
 
             worker.addEventListener(MESSAGE, onPong);
 
-            let scene = gltfBridge.getOriginalScene(createCamera()) (0)
-
-            //TODO make this exported helper function
-            const trs = { 
-                translation: [0,0,0], 
-                rotation: [0,0,0,1], 
-                scale: [1,1,1]
-            }
-            const localMatrix = getMatrixFromTrs(trs);
-            const modelMatrix = localMatrix 
-
-            const light:GltfLightNode = {
-                originalNodeId: gltfBridge.getAllNodes().length, //TODO strip this from GltfNode - make just on mesh
-                animationIds: [], //TODO make this optional
-                kind: NodeKind.LIGHT,
-                light: {
-                    kind: LightKind.Directional,
-                    color: [1,1,1],
-                    intensity: 100
-                },
-                transform: { trs, localMatrix, modelMatrix }
-            }
-
-            scene.nodes.push(light);
-
-            scene = gltfBridge.updateShaderConfigs(scene)
-
             worker.postMessage({
                 cmd: WorkerCommand.SCENE_PING,
-                scene: scene
+                scene: prepScene(gltfBridge),
+                animations: gltfBridge.getData().animations
             });
 
         }
@@ -72,14 +42,11 @@ parallel (Infinity) ([
 
 const startRenderCycle = ([worker, gltfBridge]:[Worker, GltfBridge]) => {
     let readyForUpdate = true;
-
+    
     worker.addEventListener(MESSAGE, (e:MessageEvent) => {
         switch(e.data.cmd) {
             case WorkerCommand.RENDER: {
-                const scene:GltfScene = e.data.scene;
-
-                gltfBridge.renderScene (scene);
-
+                gltfBridge.renderScene (e.data.scene);
                 readyForUpdate = true;
                 break;
             }
--- src/app/Worker.ts
+++ src/app/Worker.ts
@@ -1,40 +1,64 @@
-import {GltfScene} from "pure3d";
+import {GltfScene, gltf_createAnimator, gltf_updateScene} from "pure3d";
 import {initWorker} from "utils/Utils";
 import {WorkerCommand, MESSAGE} from "types/Worker-Types";
-import {updateState, getInitialState} from "purescript/Main/index";
+import {move} from "purescript/Main/index";
+import * as L from "partial.lenses";
 
 initWorker().fork(
     console.error,
     worker => {
 
-        let state: {
-            frameTs?: number;
-            scene: GltfScene;
-        }
+        //top-level mutable state
+        let lastTs:number;
+        let direction:number = 1;
+        let scene:Readonly<GltfScene>;
+        let updateSceneForRenderer: (frameTs: number) => (scene:GltfScene) => GltfScene;
+
+        let lightLens:Array<string | number>;
 
         worker.addEventListener(MESSAGE, (e:MessageEvent) => {
             switch(e.data.cmd) {
                 case WorkerCommand.SCENE_PING:
-                    state = {
-                        scene: e.data.scene
-                    }
 
-                    console.log(state.scene);
+                    scene = e.data.scene;
+                    updateSceneForRenderer = gltf_updateScene(gltf_createAnimator(e.data.animations) ({loop: true}));
+
+
+                    /* One day this will all be purescript :D */
+                    lightLens = [
+                        "nodes",
+                        scene.nodes.findIndex(node => node["name"] === "MyLight"),
+                        "transform",
+                        "trs",
+                        "translation",
+                        0
+                    ];
+
+                    scene = L.set(lightLens) (-15) (scene);
+
+                    setInterval(() => direction *= -1, 3000);
 
                     worker.postMessage({
                         cmd: WorkerCommand.SCENE_PONG,
                     });
                     break;
+
                 case WorkerCommand.TICK:
 
-                    /*
-                    state = state === undefined
-                        ?   getInitialState(e.data.frameTs)
-                        :   updateState (e.data.frameTs) (state); 
-                    */
+                    //Also this, would ideally all by in purescript... 
+                    if(lastTs !== undefined) {
+                        const amt = (e.data.frameTs - lastTs) * .01 * direction;
+
+                        scene = L.modify(lightLens) (move(amt)) (scene)
+                    }
+
+                    lastTs = e.data.frameTs;
+
+                    scene = updateSceneForRenderer (e.data.frameTs) (scene);
+
                     worker.postMessage({
                         cmd: WorkerCommand.RENDER,
-                        scene: state.scene
+                        scene
                     });
 
                     break;
